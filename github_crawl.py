15bc6215828a6bc1b70dbc305c4c37a05465ab74
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-05-21 18:56:11 -0500

    Added processing of user input

--- github_crawl.py
+++ github_crawl.py
@@ -2,10 +2,19 @@ import os
 import re
 import urllib
 import urllib2
+import sys
 
-for page in list(range(1, 10)):
+if len(sys.argv) != 4:
+           print "Usage: python github_crawl.py <language> <query> <pages>"
+           sys.exit()
+
+language = sys.argv[1]
+query = sys.argv[2]
+pages = sys.argv[3]
+
+for page in list(range(1, int(pages))):
            url = 'https://api.github.com/search/repositories'
-           values = {'q' : 'android+language:java',\
+           values = {'q' : query + '+language:' + language,\
                      'sort' : 'stars',\
                      'order' : 'desc',\
                      'page' :  str(page),\
